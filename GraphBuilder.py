39da5f614164d46c9a7d4e3319008334d36f5451
majdbishara <majdb594@gmail.com>
2024-11-06 13:40:44 +0200

    added support for networkx

--- GraphBuilder.py
+++ GraphBuilder.py
@@ -1,8 +1,8 @@
-import torch_geometric
 import torch
 from torch_geometric.data import Data, Dataset
-from torch_geometric.utils import dense_to_sparse
-from Similarity import Similarity
+from utils.Similarity import Similarity
+from torch_geometric.utils import to_networkx
+
 
 class GraphBuilder:
     def __init__(self, dataset: Dataset, metric: str, threshold: float = 0.8):
@@ -12,20 +12,20 @@ class GraphBuilder:
         X = dataset[:][0]
         
         self.graph = self._build_graph(X)
+        self.nx_graph = to_networkx(self.graph, to_undirected=True)
 
     def get_graph(self):
         return self.graph
+    
+    def get_nx_graph(self):
+        return self.nx_graph
 
     def _build_graph(self, X):
         # Compute the similarity (affine) matrix
         affine_matrix = self.similarity(X)
-
-        # Apply threshold to the similarity matrix
-        edge_mask = affine_matrix > self.threshold
-        edge_index, edge_attr = dense_to_sparse(edge_mask.float())
+        E = torch.vstack(torch.where(affine_matrix < self.threshold))
 
         # Create PyTorch Geometric Data object
-        data = Data(x=X, edge_index=edge_index, edge_attr=edge_attr)
-
+        data = Data(x=X, edge_index=E)
         return data
 
