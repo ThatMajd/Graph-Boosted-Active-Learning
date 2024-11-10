a91cbe47ef04fcfc77d7ad05cf7d77b19238e3d0
majdbishara <majdb594@gmail.com>
2024-11-10 14:15:45 +0200

    wandb code

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from ref.ActiveLearning_OG import AL
 from sklearn.linear_model import LogisticRegression
 from utils.dataset_wrapper import WrapperDataset
 import argparse
+import wandb
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset", type=str)
-parser.add_argument("--classifier", type=str)
-parser.add_argument("--iterations", type=int)
+parser.add_argument("--dataset", type=str, required=True)
+parser.add_argument("--classifier", type=str, required=True)
+parser.add_argument("--iterations", type=int, required=True)
 parser.add_argument("--budget_per_iter", type=int)
 parser.add_argument("--quantile", type=float)
 parser.add_argument("--sim_metric", type=str)
@@ -21,26 +22,47 @@ parser.add_argument("--use_gnn", type=bool)
 parser.add_argument("--gnn_epochs", type=int)
 parser.add_argument("--gnn_hidden", type=int, default=16)
 parser.add_argument("--plot", type=bool)
+parser.add_argument("--wandb", type=bool, default=False)
 
 args = parser.parse_args()
 
 data_object = WrapperDataset(args.dataset)
-dataset = data_object["data"]
-input_dim = data_object["dim"]
-output_dim = data_object["num_labels"]
+dataset = data_object.dataset
+input_dim = data_object.dim
+output_dim = data_object.num_labels
 
-GAL(dataset=data_object,
+gal = GAL(dataset=dataset,
     classifier=LogisticRegression(),
     budget_per_iter=args.budget_per_iter,
-    iterations=args.iterations
+    iterations=args.iterations,
     uncertainty_measures=args.uncertainty_measures,
     quantile=args.quantile,
     sim_metric=args.sim_metric,
     use_gnn=args.use_gnn,
     gnn_epochs=args.gnn_epochs,
     input_dim=input_dim,
-    hidden_dim=parser.gnn_hidden,
+    gnn_hidden=args.gnn_hidden,
     output_dim=output_dim,
     plot=args.plot,
     AL4GE=args.AL4GE,
-    n_clusters=args.n_clusters)
+    n_clusters=args.n_clusters,)
+
+res_gal = gal.run(plot=False)
+
+selection_criteria = ['random', 'custom']
+accuracy_scores_dict = {}
+for criterion in selection_criteria:
+	AL_class = AL(dataset=dataset,
+			   selection_criterion=criterion,
+			   iterations=args.iterations,
+			   budget_per_iter=args.budget_per_iter,
+			   train_limit=int(1e6),)
+	accuracy_scores_dict[criterion] = AL_class.run_pipeline()
+ 
+ 
+accuracy_scores_dict = accuracy_scores_dict | res_gal
+ 
+wandb.init()
+for step in range(args.iterations):
+    LOG = {"step": step} | {k: accuracy_scores_dict[k][step] for k in accuracy_scores_dict.keys()} 
+    wandb.log(LOG)
---
+++ run_wandb.py
@@ -0,0 +1,61 @@
+import wandb
+
+wandb_username = 'majedbishara-technion-israel-institute-of-technology'
+wandb_project = 'Bsc_Finale'
+
+command = ['${env}', '${interpreter}', 'main.py', '${args}']
+
+sweep_config={
+    'method': 'grid',
+    'metric': {
+        'name': 'Accuracy',
+        'goal': 'maximize'
+    },
+    'parameters': {
+        "dataset": {
+            "values": ["iris.pkl"]  # Replace with actual dataset options
+        },
+        "classifier": {
+            "values": ["LogisticRegression"]
+        },
+        "iterations": {
+            "values": [21]  # Adjust as needed
+        },
+        "budget_per_iter": {
+            "values": [5]
+        },
+        "quantile": {
+            "values": [0.1, 0.25, 0.5]
+        },
+        "sim_metric": {
+            "values": ["cosine", "euclidean"]  # Replace with actual similarity metrics
+        },
+        # "uncertainty_measures": {
+        #     "values": [["measure1", "measure2"], ["measure3"]]  # Replace with actual measures
+        # },
+        "AL4GE": {
+            "values": [True]
+        },
+        "n_clusters": {
+            "values": [2, 4, 5]
+        },
+        "use_gnn": {
+            "values": [True]
+        },
+        "gnn_epochs": {
+            "values": [5, 15, 25]
+        },
+        "gnn_hidden": {
+            "values": [16, 32, 64]  # Hidden layer sizes for GNN
+        },
+        'wandb': {
+            'value': 1
+        }
+    },
+    'command': command
+}
+
+sweep_id = wandb.sweep(sweep_config, project=wandb_project)
+
+print("=== Run this command ===")
+print(f"wandb agent {wandb_username}/{wandb_project}/{sweep_id}")
